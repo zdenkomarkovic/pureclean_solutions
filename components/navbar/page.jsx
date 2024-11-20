49e1b6f54d812d7b3bacfac3b427ba2a6483a4ff
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-20 01:08:54 +0100

    second commit

--- components/navbar/page.jsx
+++ components/navbar/page.jsx
@@ -1,21 +1,52 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { SlArrowDown } from "react-icons/sl";
 
 const Navbar = () => {
+  const [dropdownMenu, setDropdownMenu] = useState(true);
+
+  const toggleDropdownMenu = () => {
+    setDropdownMenu((prev) => !prev);
+  };
+
   return (
-    <header className="bg-green-100 sticky left-0 right-0 top-0 p-3">
-      <nav className="container mx-auto px-10 flex justify-between items-center ">
+    <header className="bg-green-100 sticky left-0 right-0 top-0 p-2 uppercase">
+      <nav className="container mx-auto px-10 flex justify-between items-center  ">
         <Image
           src="/pureclean_logo.jpg"
-          width={50}
-          height={50}
+          width={45}
+          height={45}
           alt="pureclean_solutions_logo"
         />
-        <Link href="#hero">O nama</Link>
-        <Link href="#ciscenje">Ciscenje</Link>
-        <Link href="#palamudjenje">Palamudjenje</Link>
-        Navbar
+        <div className="flex gap-20">
+          <div
+            onClick={toggleDropdownMenu}
+            onMouseEnter={() => setDropdownMenu(false)}
+            onMouseLeave={() => setDropdownMenu(true)}
+          >
+            <button className="flex items-center cursor-pointer uppercase">
+              Usluge{" "}
+              <SlArrowDown
+                className={`ml-2 ${dropdownMenu && "-rotate-90"} `}
+              />
+            </button>
+            <div
+              className={`flex flex-col ${
+                dropdownMenu && "hidden"
+              } absolute pt-5 gap-2`}
+            >
+              <Link href="#ciscenje">Redovno Ciscenje</Link>
+              <Link href="#ciscenje">Generalno ciscenje</Link>
+              <Link href="#ciscenje">Ciscenje posle gradjevinskih radova</Link>
+              <Link href="#ciscenje">Dubinsko pranje</Link>
+            </div>
+          </div>
+          <Link href="#hero">O nama</Link>
+        </div>
+        <Link href="#palamudjenje">Kontakt</Link>
       </nav>
     </header>
   );
--- components/palamudjenje/page.jsx
+++ components/palamudjenje/page.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Palamudjenje = () => {
   return (
-    <div
-      id="palamudjenje"
-      className="bg-red-100 py-[500px] text-[50px] text-center"
-    >
-      Palamudjenje
+    <div id="palamudjenje" className="bg-red-100 py-[500px]  text-center">
+      <h2 className="text-[50px]">Kontakt</h2>
+      <a href="tel:+381641967267">
+        <p>Pozovite nas na +381 65 9742000</p>
+        <p>ili klikom na ovo dugme</p>
+      </a>
     </div>
   );
 };
