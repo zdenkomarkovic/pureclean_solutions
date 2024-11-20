714e3253413963465104b1e58604667d12356d02
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-20 01:25:40 +0100

    3

--- components/palamudjenje/page.jsx
+++ components/palamudjenje/page.jsx
@@ -4,9 +4,21 @@ const Palamudjenje = () => {
   return (
     <div id="palamudjenje" className="bg-red-100 py-[500px]  text-center">
       <h2 className="text-[50px]">Kontakt</h2>
-      <a href="tel:+381641967267">
-        <p>Pozovite nas na +381 65 9742000</p>
-        <p>ili klikom na ovo dugme</p>
+      <p>Pozovite nas klikom na</p>
+      <a href="tel:+381659742000">
+        <button className="p-4 border-2 rounded-full">
+          Mobilni: +381 65 9742000
+        </button>
+      </a>
+      <a href="viber://chat?number=%2B381641967267">
+        <button className="p-4 border-2 rounded-full">
+          Viber: +381 65 9742000
+        </button>
+      </a>
+      <a href="https://wa.me/381641967267">
+        <button className="p-4 border-2 rounded-full">
+          WhatsApp: +381 65 9742000
+        </button>
       </a>
     </div>
   );
