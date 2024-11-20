be180013f3eda28fc4640931270ee09906111374
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-20 04:33:41 +0100

    .

--- components/hero/page.jsx
+++ components/hero/page.jsx
@@ -4,9 +4,24 @@ const Hero = () => {
   return (
     <div
       id="hero"
-      className=" border border-b-2 py-[200px] text-[50px] text-center"
+      className="relative w-full h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('/hero.jpg')" }}
     >
-      Hero
+      <div className="absolute inset-0 bg-black bg-opacity-50 z-10"> </div>
+      <div className="relative z-20 container mx-auto px-10 ">
+        <h1 className="text-[80px] text-right text-white pt-28 font-bold">
+          Pure Clean Solutions
+        </h1>
+        <div className=" w-[700px] mt-20 ml-auto text-center font-bold">
+          <h3 className="text-white text-[40px]">
+            Duboko posveceni svom poslu, izuzetno odgovorni.
+          </h3>
+          <p className="text-white text-[30px]">
+            Specijalizovani za ciscenje poslovnih objekata, kancelarija i
+            zgrada. Ali rado prihvatamo sve vrste poslova
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
