dd3785575cdd3cb80007cb4cbbaecafdfc8bed2d
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-21 11:32:59 +0100

    .

--- components/hero/page.jsx
+++ components/hero/page.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10">
         <div className="relative z-20 container mx-auto px-10 pt-20 lg:pt-40 ">
-          <h1 className="w-[60%] lg:w-[100%] pt ml-auto text-[38px] lg:text-[80px] text-left lg:text-right text-primary  font-bold">
+          <h1 className="w-[60%] lg:w-[100%] pt ml-auto text-[32px] lg:text-[80px] text-left lg:text-right text-primary  font-bold">
             Pure Clean Solutions
           </h1>
-          <div className="w-[60%] lg:w-[700px] mt-10 lg:mt-20 ml-auto text-left lg:text-center font-bold">
+          <div className="w-[60%] lg:w-[700px] mt-10 lg:mt-20 ml-auto text-center font-bold">
             <h3 className="text-[17px] text-white lg:text-[40px]">
               Duboko posveceni svom poslu, izuzetno odgovorni.
             </h3>
