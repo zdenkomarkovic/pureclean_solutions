d9a465cda9c9e8f43b886ab57f4ea86928a4411b
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-21 11:06:18 +0100

    .

--- components/hero/page.jsx
+++ components/hero/page.jsx
@@ -5,19 +5,19 @@ const Hero = () => {
   return (
     <div
       id="hero"
-      className="hidden lg:block relative w-full h-screen bg-cover bg-center"
+      className=" relative w-full h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('/hero.jpg')", zIndex: -1 }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10">
-        <div className="relative z-20 container mx-auto px-10 pt-40 ">
-          <h1 className="text-[80px] text-right text-primary pt-18 font-bold">
+        <div className="relative z-20 container mx-auto px-10  lg:pt-40 ">
+          <h1 className="text-[30px] lg:text-[80px] text-right text-primary lg:pt-18 font-bold">
             Pure Clean Solutions
           </h1>
-          <div className=" w-[700px] mt-20 ml-auto text-center font-bold">
-            <h3 className="text-white text-[40px]">
+          <div className="w-[50%] lg:w-[700px] lg:mt-20 ml-auto text-center font-bold">
+            <h3 className="text-[15px] text-white lg:text-[40px]">
               Duboko posveceni svom poslu, izuzetno odgovorni.
             </h3>
-            <p className="text-white text-[30px]">
+            <p className="text-[10px] text-white lg:text-[30px]">
               Specijalizovani za ciscenje poslovnih objekata, kancelarija i
               zgrada. Ali rado prihvatamo sve vrste poslova
             </p>
