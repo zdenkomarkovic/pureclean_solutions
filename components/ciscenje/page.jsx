4cbfa4c9f5775a2b5de7dc5cc8daf496a9c41c55
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-21 11:45:52 +0100

    .

--- components/ciscenje/page.jsx
+++ components/ciscenje/page.jsx
@@ -7,11 +7,11 @@ const Ciscenje = () => {
       id="ciscenje"
       className=" relative py-[25px] px-2 container lg:py-0 lg:static lg:pb-[100px]  mx-auto lg:px-10 text-center"
     >
-      <h2 className="absolute left-0 right-0 text-[34px] lg:static lg:text-[50px] lg:py-[80px] text-center text-primary z-20">
+      <h2 className="absolute left-0 right-0 text-[34px] lg:static lg:text-[50px] py-2 lg:py-[80px] text-center text-primary z-20">
         Čišćenje
       </h2>
       <div className=" relative lg:static lg:flex gap-20 items-center">
-        <div className="absolute bg-black opacity-50 w-full h-[50px] rounded-t-3xl lg:hidden"></div>
+        <div className="absolute bg-black opacity-50 w-full h-[55px] rounded-t-3xl lg:hidden"></div>
         <Image
           src="/ciscenje.jpg"
           width={750}
