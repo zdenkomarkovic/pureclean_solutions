cb810754cc2fb8944e7007d80e4ecda215f2f957
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-21 11:20:32 +0100

    .

--- components/hero/page.jsx
+++ components/hero/page.jsx
@@ -9,11 +9,11 @@ const Hero = () => {
       style={{ backgroundImage: "url('/hero.jpg')", zIndex: -1 }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10">
-        <div className="relative z-20 container mx-auto px-10  lg:pt-40 ">
-          <h1 className="text-[30px] lg:text-[80px] text-right text-primary lg:pt-18 font-bold">
+        <div className="relative z-20 container mx-auto px-10 pt-20 lg:pt-40 ">
+          <h1 className="w-[50%] lg:w-[100%] pt ml-auto text-[30px] lg:text-[80px] text-right text-primary  font-bold">
             Pure Clean Solutions
           </h1>
-          <div className="w-[50%] lg:w-[700px] lg:mt-20 ml-auto text-center font-bold">
+          <div className="w-[50%] lg:w-[700px] mt-10 lg:mt-20 ml-auto text-center font-bold">
             <h3 className="text-[15px] text-white lg:text-[40px]">
               Duboko posveceni svom poslu, izuzetno odgovorni.
             </h3>
