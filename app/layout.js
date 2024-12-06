8b944f267b19b95f8c77e9182f1c8cb5f51833b3
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-06 21:46:20 +0100

    .

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="sr">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
