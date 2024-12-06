d2882a53cb707f2ff7aa8c25e3f2ef0a8238e924
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-06 21:07:46 +0100

    .

--- components/hero/page.jsx
+++ components/hero/page.jsx
@@ -17,8 +17,12 @@ const TypingEffect = ({ text, speed = 10 }) => {
   }, [index, text, speed]);
 
   return (
-    <h1 className="w-[55%] lg:w-[100%] ml-auto text-[38px] lg:text-[80px] text-left lg:text-right text-primary font-bold transition-opacity duration-700">
-      {displayedText}
+    <h1 className="relative flex flex-col pl-[150px] lg:pl-0 lg:w-[100%] ml-auto text-[38px] lg:text-[80px] text-left lg:text-right text-primary font-bold transition-opacity duration-700">
+      {/* {displayedText} */}
+      Pure Clean Solutions.
+      <span className=" absolute left-0 top-[150px] animate-colorChange text-center text-[30px] lg:text-[80px]">
+        Čišćenje prepustite nama, jer smo mi profesionalci
+      </span>
     </h1>
   );
 };
@@ -32,14 +36,13 @@ const Hero = () => {
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10">
         <div className="relative z-20 container mx-auto px-2 lg:px-10 pt-20 lg:pt-40 ">
-          {/* <h1 className="w-[55%] lg:w-[100%] ml-auto text-[38px] lg:text-[80px] text-left lg:text-right text-primary font-bold">
-            Pure Clean Solutions
-          </h1> */}
-          <TypingEffect text="Pure Clean Solutions" speed={50} />
-          <div className="w-[55%] lg:w-[700px] mt-10 lg:mt-20 ml-auto text-center font-bold">
-            <h3 className="text-[17px] text-white lg:text-[40px] opacity-0 animate-fade-in">
-              Čišćenje Niš
-            </h3>
+          <h1 className="w-[55%] lg:w-[100%] ml-auto text-[38px] lg:text-[80px] text-left lg:text-right text-primary font-bold">
+            Pure Clean Solutions. Čišćenje prepustite nama, jer smo mi
+            profesionalci
+          </h1>
+          {/* <TypingEffect text="Pure Clean Solutions" speed={50} /> */}
+          <div className="w-[55%] lg:w-[700px] mt-[200px] lg:mt-[300px] ml-auto text-center font-bold">
+            <h3 className="text-[17px] text-white lg:text-[40px] opacity-0 animate-fade-in"></h3>
             <p className="text-[14px] mt-3 text-white  lg:text-[30px] opacity-0 animate-fade-in2">
               Specijalizovani za čišćenje poslovnih objekata, kancelarija i
               zgrada. Ali rado prihvatamo sve vrste poslova
