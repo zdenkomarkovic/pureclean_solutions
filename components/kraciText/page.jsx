b7abe1a60eac52eb770a9d8fd6c8bd8f205f290a
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-07 02:10:30 +0100

    .

--- components/kraciText/page.jsx
+++ components/kraciText/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const KraciText = ({ text }) => {
-  const skracenText = text.substring(0, 120) + "...";
+  const skracenText = text.substring(0, 90) + "...";
   console.log(text);
   return <p>{skracenText}</p>;
 };
