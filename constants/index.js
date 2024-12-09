12000e7e42d16f23b324ec88fea25d0288b86e8a
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-09 02:35:38 +0100

    .

--- constants/index.js
+++ constants/index.js
@@ -61,3 +61,60 @@ export const pageData = [
 export const getPageData = (title) => {
   return pageData.find((page) => page.title === title);
 };
+
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: "https://www.ciscenje-nis.rs", // Zameni svojom domenom
+//   generateRobotsTxt: true,
+//   robotsTxtOptions: {
+//     policies: [
+//       {
+//         userAgent: "*",
+//         allow: "/",
+//       },
+//     ],
+//   },
+// };
+// next-sitemap.config.js
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true, // Preporučuje se za otkrivanje problema
+
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/ciscenje-nis", // SEO-friendly URL
+//         destination: "/ciscenje",
+//       },
+//       {
+//         source: "/generalno-ciscenje-nis", // Još jedan primer
+//         destination: "/generalnociscenje",
+//       },
+//       {
+//         source: "/ciscenje-posle-gradjevinskih-radova", // Još jedan primer
+//         destination: "/ciscenjeposlegradjevinskihradova",
+//       },
+//       {
+//         source: "/dubinsko-pranje-nis", // Još jedan primer
+//         destination: "/dubinskopranje",
+//       },
+//     ];
+//   },
+//   async redirects() {
+//     return [
+//       {
+//         source: "/old-route",
+//         destination: "/new-route",
+//         permanent: true, // 301 redirekcija za SEO
+//       },
+//     ];
+//   },
+//   env: {
+//     SITE_URL: "https://www.ciscenje-nis.rs", // Korisno za sitemap generaciju
+//   },
+// };
+
+// export default nextConfig;
+
+// next.config.mjs
