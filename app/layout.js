cc1f2f3c91df62344e94d7e116748590a975e1ee
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-09 20:33:17 +0100

    .

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="sr">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0,  maximum-scale=1.0, user-scalable=no"
+        />
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
