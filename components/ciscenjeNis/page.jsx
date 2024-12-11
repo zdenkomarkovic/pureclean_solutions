d381e68214cb802b9e6c831c6ed9ad87ca58546a
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-11 02:17:57 +0100

    .

--- components/ciscenjeNis/page.jsx
+++ components/ciscenjeNis/page.jsx
@@ -8,22 +8,68 @@ import { FaWhatsappSquare } from "react-icons/fa";
 const CiscenjeNis = () => {
   return (
     <div className=" bg-black ">
-      <div className=" container pb-[50px] lg:pb-[120px] text-center mx-auto">
+      <div className=" container lg:px-[50px] pb-[50px] lg:pb-[120px] text-center mx-auto">
         <h2 className="text-[37px] py-[25px] lg:text-[78px] lg:py-[60px] text-center  text-primary">
           Pure Clean Solutions Čišćenje Niš
         </h2>
-        <p className="text-[20px] mb-20 text-white  lg:text-[30px]">
+        <p className="text-[20px]  text-white  lg:text-[30px]">
           Pozovite{" "}
           <span className="text-primary font-bold">Pure Clean Solutions</span> i
-          zakazite profesionalno čišćenje vašeg prostora! Naša agencija za
-          čišćenje nudi usluge koje prilagođavamo vašim potrebama, bilo da se
-          radi o stambenim ili poslovnim objektima, kancelarijama ili zgradama.
-          Obraćamo pažnju na detalje i koristimo visokokvalitetna sredstva za
-          čišćenje. Garantujemo savršenu čistoću i osveženje vašeg prostora.
-          Prepustite nama brigu o higijeni, dok vi završavate druge obaveze.
-          Kontaktirajte nas i uverite se zašto smo među najpouzdanijim
-          agencijama za čišćenje. Vaše zadovoljstvo je naša najveća motivacija!
+          zakazite profesionalno čišćenje vašeg prostora. Naša agencija za
+          čišćenje specijalizovana je za pružanje visokokvalitetnih usluga
+          čišćenja koje se mogu prilagoditi svim vašim potrebama. Bilo da se
+          radi o stambenim prostorima, poslovnim objektima, kancelarijama,
+          industrijskim zgradama ili drugim vrstama prostora, mi smo tu da vam
+          pomognemo da očuvate čistoću i higijenu na najvišem nivou. Naš tim
+          stručnjaka koristi najsavremenija sredstva i opremu, a svaki zadatak
+          obavljamo sa pažnjom na detalje i potpunu posvećenost.
         </p>
+        <p className="text-[20px] text-white  lg:text-[30px]">
+          Naša ponuda uključuje različite usluge čišćenja, uključujući
+          svakodnevno, dubinsko čišćenje, čišćenje nakon renoviranja, kao i
+          specijalizovane usluge kao što su čišćenje tepiha, prozora i
+          nameštaja. Bez obzira na vrstu čišćenja, mi se uvek trudimo da se
+          prilagodimo vašim specifičnim zahtevima. Naš tim je obučen da koristi
+          ekološki prihvatljive i sigurne proizvode koji garantuju visok nivo
+          efikasnosti i zaštitu vašeg zdravlja i imovine.
+        </p>
+        <p className="text-[20px] text-white  lg:text-[30px]">
+          Naša agencija je poznata po tome što se brine o svim aspektima
+          čišćenja, od redovnog održavanja do dubinskog čišćenja prostora koji
+          nisu često u upotrebi. Ako vam je potrebna hitna usluga ili želite
+          dogovoriti redovno čišćenje, možete se osloniti na nas da će posao
+          biti obavljen brzo i efikasno. Sve naše usluge su fleksibilne, tako da
+          možemo raditi u skladu sa vašim rasporedom i specifičnim potrebama.
+        </p>
+        <p className="text-[20px]  text-white  lg:text-[30px]">
+          Kada izaberete{" "}
+          <span className="text-primary font-bold">Pure Clean Solutions</span>,
+          birate pouzdanu kompaniju koja ne pravi kompromis kada je u pitanju
+          kvalitet. Kroz naše profesionalne usluge čišćenja, vaš prostor će uvek
+          izgledati besprekorno, sa dugotrajnim efektima. Garantujemo da ćete
+          biti zadovoljni rezultatima, jer nam je važno da svaki prostor koji
+          očistimo bude besprekorno čist i osvežen. Naš cilj je da vaš prostor
+          bude zdrav, siguran i prijatan za boravak ili rad.
+        </p>
+        <p className="text-[20px] text-white  lg:text-[30px]">
+          Kontaktirajte nas danas i uverite se zašto su mnogi naši klijenti
+          odabrali nas kao svoju primarnu agenciju za čišćenje. Sa{" "}
+          <span className="text-primary font-bold">Pure Clean Solutions</span>,
+          vaš prostor je u sigurnim rukama. Vaše zadovoljstvo je naša najveća
+          motivacija, i trudićemo se da pružimo najbolju moguću uslugu koja će
+          prevazići vaša očekivanja. Svi naši radnici su obučeni i spremni da
+          reše čak i najzahtevnije zadatke čišćenja, tako da možete biti sigurni
+          da ćemo se pobrinuti za svaki detalj.
+        </p>
+        <p className="text-[20px] mb-20 text-white  lg:text-[30px]">
+          Bez obzira na to da li vam je potrebna usluga za poslovne prostore ili
+          vašu kuću, možete računati na nas da ćemo obaviti posao sa pažnjom,
+          efikasnošću i visokim standardima.{" "}
+          <span className="text-primary font-bold">Pure Clean Solutions</span>{" "}
+          je vaša pouzdana agencija za čišćenje u Nišu koja pruža vrhunske
+          usluge u svim oblastima čišćenja.
+        </p>
+
         <div className="">
           <div className="flex flex-col lg:flex-row mx-auto justify-center gap-3 lg:gap-10 items-center">
             <a href="tel:+381631210359">
